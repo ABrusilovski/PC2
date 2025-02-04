02ae669a5acebaaf3baf3105048fd2b03a6e4433
M37I8SA\melis <melissabangloy023@gmail.com>
2025-02-04 11:54:35 -0500

    implemented inheritance : Memberhsip (adult, child, student, senior) and AdditionalOption(yoga, karate, trainer) // TODO: need to fix namespace

--- FitnessMemberships/Form1.cs
+++ FitnessMemberships/Form1.cs
@@ -8,58 +8,138 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//TODO need to fix namespace problem
 namespace My_First_Program
 {
+
+    //---MEMBERSHIPS
+    public abstract class Membership
+    {
+        public double BaseRate { get; protected set; }
+
+        public virtual double GetBaseMembershipFee()
+        {
+            return BaseRate;
+        }
+    }
+
+    // abstract class for memberships
+    public class AdultMembership : Membership
+    {
+        public AdultMembership()
+        {
+            BaseRate = 25.0;
+        }
+    }
+
+    public class ChildMembership : Membership
+    {
+        public ChildMembership()
+        {
+            BaseRate = 10.0;
+        }
+    }
+
+    public class StudentMembership : Membership
+    {
+        public StudentMembership()
+        {
+            BaseRate = 15.0;
+        }
+    }
+
+    public class SeniorMembership : Membership
+    {
+        public SeniorMembership()
+        {
+            BaseRate = 12.5;
+        }
+    }
+    
+    
+    //----OPTIONS
+    //abstact class for AdditionalOption
+    public abstract class AdditionalOption
+    {
+        public abstract double GetFee();
+    }
+
+    // Concrete classes for each option
+    public class Yoga : AdditionalOption
+    {
+        public override double GetFee()
+        {
+            return 20.0; // Yoga fee
+        }
+    }
+
+    public class Karate : AdditionalOption
+    {
+        public override double GetFee()
+        {
+            return 30.0; // Karate fee
+        }
+    }
+
+    public class Trainer : AdditionalOption
+    {
+        public override double GetFee()
+        {
+            return 25.0; // Trainer fee
+        }
+    }
+
+
+    //FORM
     public partial class Form1 : Form
     {
-        private readonly double ADULT_RATE = 25.0;
-        private readonly double CHILD_RATE = 10.0;
-        private readonly double STUDENT_RATE = 15.0;
-        private readonly double SENIOR_RATE = 12.5;
-        private readonly double YOGA_FEE = 20.0;
-        private readonly double KARATE_FEE = 30.0;
-        private readonly double TRAINER_FEE = 25.0;
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Button click event for calculating fees
         private void calcButton_Click(object sender, EventArgs e)
         {
             //months validation
-            //ideally minimum of 1 month and yearly renewal
-            if (!int.TryParse(monthsTextBox.Text, out int months) || months < 1 || months >12)
+            if (!int.TryParse(monthsTextBox.Text, out int months) || months < 1 || months > 12)
             {
                 monthsTextBox.Text = "";
                 MessageBox.Show("Invalid entry of membership length. \nPlease enter a value between 1 and 12.");
                 return; // stop execution if input is invalid
-
             }
-            //test
-            double monthlyFee = GetBaseMembershipFee();
-            monthlyFee += GetAdditionalOptionsFee();
 
+            Membership membership = GetMembershipType();
+            double baseFee = membership.GetBaseMembershipFee();
+            double additionalFee = GetAdditionalOptionsFee();
 
+            double monthlyFee = baseFee + additionalFee;
             double totalFee = monthlyFee * months;
 
             monthlyFeeDisplay.Text = monthlyFee.ToString("c");
             totalFeeDisplay.Text = totalFee.ToString("c");
         }
-        //helper method to get membership fee
-        private double GetBaseMembershipFee()
+
+        // Get the type of membership selected
+        private Membership GetMembershipType()
         {
-            if (adultRadioButton.Checked) return ADULT_RATE;
-            if (childRadioButton.Checked) return CHILD_RATE;
-            if (studentRadioButton.Checked) return STUDENT_RATE;
-            return SENIOR_RATE; // Default to Senior Rate
+            if (adultRadioButton.Checked) return new AdultMembership();
+            if (childRadioButton.Checked) return new ChildMembership();
+            if (studentRadioButton.Checked) return new StudentMembership();
+            return new SeniorMembership(); // Default to Senior Membership
         }
 
-        //helper method to calculate additional options fee
-        private double GetAdditionalOptionsFee() {
+        // Get the total additional fee for options selected
+        private double GetAdditionalOptionsFee()
+        {
             double additionalFee = 0;
-            if (yogaCheckBox.Checked) additionalFee += YOGA_FEE;
-            if (karateCheckBox.Checked) additionalFee += KARATE_FEE;
-            if (trainerCheckBox.Checked) additionalFee += TRAINER_FEE;
+            
+            if (yogaCheckBox.Checked) additionalFee += (new Yoga().GetFee());
+            if (karateCheckBox.Checked) additionalFee += (new Karate().GetFee());
+            if (trainerCheckBox.Checked) additionalFee += (new Trainer().GetFee());
+
+           
+
             return additionalFee;
         }
 
---
+++ FitnessMemberships/Membership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace My_First_Program
+{
+    public class Membership
+    {
+        public double MonthlyRate { get; set; }
+
+        public Membership(double rate)
+        {
+            MonthlyRate = rate;
+        }
+
+        public virtual double CalculateMonthlyFee()
+        {
+            return MonthlyRate;
+        }
+    }
+}
+
